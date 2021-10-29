4e3071958640444800a5435c0d6de0abd15cbbc6
ugurctaygun <ugurctaygun@gmail.com>
2021-10-29 22:39:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//Routes
+app.use("/api", require("./routes"));
+
+//Enable cors
+app.use(cors);
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
