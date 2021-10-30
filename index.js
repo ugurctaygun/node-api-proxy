0bb3d91eea07d43ae2a41b4c1138c06e3c0670df
ugurctaygun <ugurctaygun@gmail.com>
2021-10-30 14:10:01 +0300

    proxy , rate limit , cache

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 3,
+});
+app.use(limiter), app.set("trust proxy", 1);
+
+app.use(express.static("public"));
 
 //Routes
 app.use("/api", require("./routes"));
